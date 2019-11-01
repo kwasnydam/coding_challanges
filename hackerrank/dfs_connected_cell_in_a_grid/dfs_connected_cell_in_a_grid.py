c22b8e7d6f0400c1b73a0fa52d98724718eb67f4
Damian Kwasny <damiankwasny95@gmail.com>
2019-11-01 12:30:44 +0100

    refactored the logic code and removed debug prints

--- hackerrank/dfs_connected_cell_in_a_grid/dfs_connected_cell_in_a_grid.py
+++ hackerrank/dfs_connected_cell_in_a_grid/dfs_connected_cell_in_a_grid.py
@@ -1,10 +1,10 @@
 import os
 
+
 def maxRegion(grid):
     seen = set()
     max_region = 0
-    grid_width = len(grid[0])
-    grid_height = len(grid)
+    grid_width, grid_height = get_grid_dimensions(grid)
     for x in range(grid_height):
         for y in range(grid_width):
             start = (x, y)
@@ -16,12 +16,10 @@ def maxRegion(grid):
 
 def region(grid, coordinates, seen):
     if decode(coordinates) not in seen:
-        print('visiting node {}'.format(coordinates))
         seen.add(decode(coordinates))
     else:
         return 0
-    x = coordinates[0]
-    y = coordinates[1]
+    x, y = coordinates
     current_val = grid[x][y]
     if current_val == 0:
         return 0
@@ -34,18 +32,24 @@ def decode(coordinates):
 
 def get_valid_neighbours(grid, coordinates, seen):
     valid_neighbours = []
-    grid_width = len(grid[0])
-    grid_height = len(grid)
-    x = coordinates[0]
-    y = coordinates[1]
+    x, y = coordinates
     for i in range(-1, 2):
         for j in range(-1, 2):
-            # print(x+i, y+j)
-            if x + i >= 0 and x + i < grid_height and y + j >= 0 and y + j < grid_width and not [x + i, y + j] == [x, y]:
-                # print('appending {}'.format((x+i, y+j)))
+            if is_valid_neighbour(x, y, i, j, grid):
                 valid_neighbours.append((x+i, y+j))
-    print('For coor = {} valid valid_neighbours = {}'.format(coordinates, valid_neighbours))
     return valid_neighbours
+
+def is_valid_neighbour(x, y, i, j, grid):
+    grid_width, grid_height = get_grid_dimensions(grid)
+    x_i_coord_within_border = x + i >= 0 and x + i < grid_height
+    y_i_coord_within_border = y + j >= 0 and y + j < grid_width
+    i_j_not_both_zero = not [x + i, y + j] == [x, y]
+    return x_i_coord_within_border and y_i_coord_within_border and i_j_not_both_zero
+
+def get_grid_dimensions(grid):
+    grid_width = len(grid[0])
+    grid_height = len(grid)
+    return grid_width, grid_height
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
